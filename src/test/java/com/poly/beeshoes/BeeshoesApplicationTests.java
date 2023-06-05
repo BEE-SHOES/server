bc86a81cc3fe933710cf9ec4aa12f167ad09a981
nguyencongthang2509 <congthang25092003@gmail.com>
2023-06-06 02:52:36 +0700

    first commit

---
+++ src/test/java/com/poly/beeshoes/BeeshoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.beeshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeshoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
