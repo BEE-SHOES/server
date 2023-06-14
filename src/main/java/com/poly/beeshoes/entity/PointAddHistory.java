3d665ba4ff09c50c9ddc022613c222294e0f904e
nguyencongthang2509 <congthang25092003@gmail.com>
2023-06-14 14:31:47 +0700

    no message

--- src/main/java/com/poly/beeshoes/entity/PointAddHistory.java
+++ src/main/java/com/poly/beeshoes/entity/PointAddHistory.java
@@ -34,5 +34,5 @@ public class PointAddHistory extends PrimaryEntity {
     private Bill bill;
 
     @Column
-    private Integer accumulatedPoints;
+    private Integer accumulatedPoints; // điểm tích được ở hóa đơn
 }
